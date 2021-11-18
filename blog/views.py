02bd80fb5aecfc54aeac22193017c0a020b9e2aa
srintikayunikharisma <srintikayk1006@gmail.com>
2021-11-18 23:01:39 +0700

    Menambah register dan database

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
+from .models import Post
 
 def home_view(request):
 	context= {}
+
+	post = Post.objects.all()
+	context["posts"] = post
 
 	return render(request,"home.html",context)
 
 def about_view(request):
 	context= {}
-
+	context["nama"] = "Srintika"
+	context["asal"] = "Bukitinggi"
+	context["tinggi"] = "154"
+	context["bb"] = "42"
 	return render(request,"about.html",context)
--- django_project/urls.py
+++ django_project/urls.py
@@ -17,9 +17,11 @@ from django.contrib import admin
 from django.urls import path
 
 from blog.views import home_view, about_view
+from users.views import register
 
 urlpatterns = [
 path('', home_view, name="blog-home"),
 path('about/', about_view, name="blog-about"),
     path('admin/', admin.site.urls),
+    path('register/',register,name="register")
 ]
