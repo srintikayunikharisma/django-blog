123a9f76706c0a82e4dfa9c9ffaf2c30349d76a2
srintikayunikharisma <srintikayk1006@gmail.com>
2021-11-25 22:53:35 +0700

    Menambahkan CRUD Profile

--- django_project/urls.py
+++ django_project/urls.py
@@ -19,7 +19,10 @@ from django.contrib.auth import views as auth_views
 from django.urls import path, include 
 
 from blog.views import home_view, about_view
-from users.views import register
+from users.views import register, profile
+
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('blog.urls')),
@@ -27,5 +30,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/',register,name="register"),
     path('login/', auth_views.LoginView.as_view(template_name="users/login.html"), name="login"),
-    path('logout/', auth_views.LogoutView.as_view(template_name="users/logout.html"), name="logout")
+    path('logout/', auth_views.LogoutView.as_view(template_name="users/logout.html"), name="logout"),
+    path('profile/', profile, name='profile'),
 ] 
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
